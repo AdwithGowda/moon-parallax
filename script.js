0c3d136dbe66b0a482949189775a5ad25b506feb
AdwithGowda <126180447+AdwithGowda@users.noreply.github.com>
2023-12-28 22:59:11 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,1 @@
-text="text/javascript">
-let = document.getElementById("bg");
-let moon = document.getElementById("moon");
-let mountain = document.getElementById("mountain");
-let road= document.getElementById("road");
-let text = document.getElementById("text");
 
-window.addEventListener('scroll',function(){
-    console.log('Scroll event detected');
-    var value = window.scrollY;
-    bg.style.top = value * 0.5 +'px';
-    moon.style.left = -value * 0.5 +'px';
-    mountain.style.top = -value * 0.15 +'px';
-
-})
